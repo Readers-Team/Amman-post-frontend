b8cef448adb98e51258e7f76ae2d29dbacb2e04e
Osamamomani1 <momaniosama97@gmail.com>
2021-07-14 22:11:33 +0300

    done

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ ReactDOM.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-    redirectUri={`https://60ef1983d0f7910007ea004e--kind-raman-c031a4.netlify.app/community`}
+    redirectUri={window.location.origin}
   >
     <App />
   </Auth0Provider>,
